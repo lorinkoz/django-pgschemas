c565ed6880de2b62b5237185b29d376d99639f40
Lorenzo Peña <lorinkoz@gmail.com>
2019-01-04 11:34:29 -0500

    Enhanced interactivity and scope of tenant commands

--- django_pgschemas/management/commands/__init__.py
+++ django_pgschemas/management/commands/__init__.py
@@ -1,8 +1,11 @@
+from enum import Flag
+
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 from ._executors import sequential, parallel
 from ...utils import get_tenant_model, create_schema
+from ...volatile import VolatileTenant
 
 WILDCARD_ALL = ":all:"
 WILDCARD_STATIC = ":static:"
@@ -11,19 +14,26 @@ WILDCARD_DYNAMIC = ":dynamic:"
 EXECUTORS = {"sequential": sequential, "parallel": parallel}
 
 
+class SchemaScope(Flag):
+    STATIC = 1
+    DYNAMIC = 2
+    ALL = 3
+
+
 class WrappedSchemaOption(object):
-    allow_static = True
-    allow_dynamic = True
+    interactive = True
+    scope = SchemaScope.ALL
 
     def add_arguments(self, parser):
         parser.add_argument("-s", "--schema", dest="schema", help="Schema to execute the current command")
-        parser.add_argument(
-            "--noinput",
-            "--no-input",
-            action="store_false",
-            dest="interactive",
-            help="Tells Django to NOT prompt the user for input of any kind.",
-        )
+        if self.interactive:
+            parser.add_argument(
+                "--noinput",
+                "--no-input",
+                action="store_false",
+                dest="interactive",
+                help="Tells Django to NOT prompt the user for input of any kind.",
+            )
         parser.add_argument(
             "--executor",
             dest="executor",
@@ -51,39 +61,45 @@ class WrappedSchemaOption(object):
 
     def _get_schemas_from_options(self, **options):
         schema = options.get("schema", "")
+        allow_static = self.scope & SchemaScope.STATIC
+        allow_dynamic = self.scope & SchemaScope.DYNAMIC
 
         if not schema:
-            if options["interactive"]:
-                schema = input("Enter schema to run command (leave blank for running on 'all' schemas): ").strip()
+            if not self.interactive:
+                schema = WILDCARD_ALL
+            elif options["interactive"]:
+                schema = input(
+                    "Enter schema to run command (leave blank for running on '%s' schemas): " % self.scope.name
+                ).strip()
                 if not schema:
                     schema = WILDCARD_ALL
             else:
                 raise CommandError("No schema provided")
 
         TenantModel = get_tenant_model()
-        static_schemas = [x for x in settings.TENANTS.keys() if x != "default"] if self.allow_static else []
-        dynamic_schemas = TenantModel.objects.values_list("schema_name", flat=True) if self.allow_dynamic else []
+        static_schemas = [x for x in settings.TENANTS.keys() if x != "default"] if allow_static else []
+        dynamic_schemas = TenantModel.objects.values_list("schema_name", flat=True) if allow_dynamic else []
 
         if schema == WILDCARD_ALL:
-            if not self.allow_static and not self.allow_dynamic:
+            if not allow_static and not allow_dynamic:
                 raise CommandError("Schema wildcard %s is now allowed" % WILDCARD_ALL)
             return static_schemas + list(dynamic_schemas)
         elif schema == WILDCARD_STATIC:
-            if not self.allow_static:
+            if not allow_static:
                 raise CommandError("Schema wildcard %s is now allowed" % WILDCARD_STATIC)
             return static_schemas
         elif schema == WILDCARD_DYNAMIC:
-            if not self.allow_dynamic:
+            if not allow_dynamic:
                 raise CommandError("Schema wildcard %s is now allowed" % WILDCARD_DYNAMIC)
             return list(dynamic_schemas)
-        elif schema in settings.TENANTS and schema != "default" and self.allow_static:
+        elif schema in settings.TENANTS and schema != "default" and allow_static:
             return [schema]
-        elif TenantModel.objects.filter(schema_name=schema).exists() and self.allow_dynamic:
+        elif TenantModel.objects.filter(schema_name=schema).exists() and allow_dynamic:
             return [schema]
 
         domain_matching_schemas = []
 
-        if self.allow_static:
+        if allow_static:
             domain_matching_schemas += [
                 schema_name
                 for schema_name, data in settings.TENANTS.items()
@@ -91,7 +107,7 @@ class WrappedSchemaOption(object):
                 and any([x for x in data["DOMAINS"] if x.startswith(schema)])
             ]
 
-        if self.allow_dynamic:
+        if allow_dynamic:
             domain_matching_schemas += (
                 TenantModel.objects.filter(domains__domain__istartswith=schema)
                 .distinct()
@@ -108,20 +124,30 @@ class WrappedSchemaOption(object):
         return domain_matching_schemas
 
 
-class DynamicTenantCommand(WrappedSchemaOption, BaseCommand):
-    allow_static = False
-    allow_dynamic = True
-
+class TenantCommand(WrappedSchemaOption, BaseCommand):
     def handle(self, *args, **options):
         schemas = self.get_schemas_from_options(**options)
         executor = self.get_executor_from_options(**options)
         executor(schemas, type(self), "_raw_handle_tenant", args, options, pass_schema_in_kwargs=True)
 
     def _raw_handle_tenant(self, *args, **kwargs):
-        TenantModel = get_tenant_model()
         schema_name = kwargs.pop("schema_name")
-        tenant = TenantModel.objects.get(schema_name=schema_name)
-        self.handle_tenant(tenant, *args, **kwargs)
+        if schema_name in settings.TENANTS:
+            domains = settings.TENANTS[schema_name].get("DOMAINS", [])
+            tenant = VolatileTenant.create(schema_name=schema_name, domain_url=domains[0] if domains else None)
+            self.handle_tenant(tenant, *args, **kwargs)
+        else:
+            TenantModel = get_tenant_model()
+            tenant = TenantModel.objects.get(schema_name=schema_name)
+            self.handle_tenant(tenant, *args, **kwargs)
 
     def handle_tenant(self, tenant, *args, **options):
         pass
+
+
+class StaticTenantCommand(TenantCommand):
+    scope = SchemaScope.STATIC
+
+
+class DynamicTenantCommand(TenantCommand):
+    scope = SchemaScope.DYNAMIC
--- django_pgschemas/management/commands/migrate_schemas.py
+++ django_pgschemas/management/commands/migrate_schemas.py
@@ -2,11 +2,19 @@ from django.core import management
 from django.core.management.base import BaseCommand
 
 from . import WrappedSchemaOption
+from .runschema import Command as RunSchemaCommand
+
+
+class NonInteractiveRunSchemaCommand(RunSchemaCommand):
+    interactive = False
 
 
 class MigrateSchemasCommand(WrappedSchemaOption, BaseCommand):
+    interactive = False
+
     def handle(self, *args, **options):
-        management.call_command("runschema", "django.core.migrate", *args, **options)
+        runschema = NonInteractiveRunSchemaCommand()
+        management.call_command(runschema, "django.core.migrate", *args, **options)
 
 
 Command = MigrateSchemasCommand

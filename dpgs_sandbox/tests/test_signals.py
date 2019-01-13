6fd114070b36b446a18f183d1616286fb1cec29b
Lorenzo Peña <lorinkoz@gmail.com>
2019-01-12 21:16:24 -0500

    Added tests for signals module

---
+++ dpgs_sandbox/tests/test_signals.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from django_pgschemas.utils import get_tenant_model, schema_exists
+
+
+class SignalsTestCase(TestCase):
+    """
+    Tests signals.
+    """
+
+    def test_tenant_delete_callback(self):
+        TenantModel = get_tenant_model()
+        TenantModel.auto_create_schema = False
+        TenantModel.auto_drop_schema = True
+        tenant = TenantModel(schema_name="tenant1")
+        tenant.save()
+        tenant.create_schema(sync_schema=False)
+        self.assertTrue(schema_exists("tenant1"))
+        TenantModel.objects.all().delete()
+        self.assertTrue(not schema_exists("tenant1"))

1d7d942b6244d45b7afe2b0fa7d0ae68084cb3ee
Lorenzo Peña <lorinkoz@gmail.com>
2020-04-14 22:31:56 -0400

    Enhanced messages

--- django_pgschemas/checks.py
+++ django_pgschemas/checks.py
@@ -20,21 +20,36 @@ def check_apps(app_configs, **kwargs):
     user_app = get_user_model()._meta.app_config.name
     session_apps = list(get_session_apps())
     if "django.contrib.contenttypes" in settings.TENANTS["default"].get("APPS", []):
-        errors.append(checks.Warning("'django.contrib.contenttypes' must be on 'public' schema.", id="pgschemas.W001",))
+        errors.append(
+            checks.Warning(
+                "'django.contrib.contenttypes' in TENANTS['default']['APPS'] must be on 'public' schema only.",
+                id="pgschemas.W001",
+            )
+        )
     for schema in settings.TENANTS:
         schema_apps = settings.TENANTS[schema].get("APPS", [])
         if schema not in ["public", "default"]:
             if "django.contrib.contenttypes" in schema_apps:
                 errors.append(
-                    checks.Warning("'django.contrib.contenttypes' must be on 'public' schema.", id="pgschemas.W001",)
+                    checks.Warning(
+                        "'django.contrib.contenttypes' in TENANTS['%s']['APPS'] must be on 'public' schema only."
+                        % schema,
+                        id="pgschemas.W001",
+                    )
                 )
         for session_app in session_apps:
-            if (session_app in schema_apps and user_app not in schema_apps) or (
-                user_app in schema_apps and session_app not in schema_apps and session_app in settings.INSTALLED_APPS
-            ):
+            if session_app in schema_apps and user_app not in schema_apps:
                 errors.append(
                     checks.Warning(
-                        "'%s' must be on schemas that also have '%s'." % (session_app, user_app), id="pgschemas.W002",
+                        "'%s' must be together with '%s' in TENANTS['%s']['APPS']." % (user_app, session_app, schema),
+                        id="pgschemas.W002",
+                    )
+                )
+            elif user_app in schema_apps and session_app not in schema_apps and session_app in settings.INSTALLED_APPS:
+                errors.append(
+                    checks.Warning(
+                        "'%s' must be together with '%s' in TENANTS['%s']['APPS']." % (session_app, user_app, schema),
+                        id="pgschemas.W002",
                     )
                 )
     return errors

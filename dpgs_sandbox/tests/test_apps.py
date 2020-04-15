789e03db29679e9a946f97543b59e19f9e9a9c79
Lorenzo Peña <lorinkoz@gmail.com>
2020-04-14 22:31:56 -0400

    Enhanced messages

--- dpgs_sandbox/tests/test_apps.py
+++ dpgs_sandbox/tests/test_apps.py
@@ -143,13 +143,19 @@ class AppConfigTestCase(TestCase):
         with override_settings(TENANTS={"default": {"APPS": ["django.contrib.contenttypes"]}}):
             errors = check_apps(self.app_config)
             expected_errors = [
-                checks.Warning("'django.contrib.contenttypes' must be on 'public' schema.", id="pgschemas.W001",)
+                checks.Warning(
+                    "'django.contrib.contenttypes' in TENANTS['default']['APPS'] must be on 'public' schema only.",
+                    id="pgschemas.W001",
+                )
             ]
             self.assertEqual(errors, expected_errors)
         with override_settings(TENANTS={"default": {}, "www": {"APPS": ["django.contrib.contenttypes"]}}):
             errors = check_apps(self.app_config)
             expected_errors = [
-                checks.Warning("'django.contrib.contenttypes' must be on 'public' schema.", id="pgschemas.W001",)
+                checks.Warning(
+                    "'django.contrib.contenttypes' in TENANTS['www']['APPS'] must be on 'public' schema only.",
+                    id="pgschemas.W001",
+                )
             ]
             self.assertEqual(errors, expected_errors)
 
@@ -160,7 +166,9 @@ class AppConfigTestCase(TestCase):
             errors = check_apps(self.app_config)
             expected_errors = [
                 checks.Warning(
-                    "'django.contrib.sessions' must be on schemas that also have '%s'." % user_app, id="pgschemas.W002",
+                    "'%s' must be together with '%s' in TENANTS['%s']['APPS']."
+                    % (user_app, "django.contrib.sessions", "default"),
+                    id="pgschemas.W002",
                 )
             ]
             self.assertEqual(errors, expected_errors)
@@ -173,7 +181,9 @@ class AppConfigTestCase(TestCase):
             errors = check_apps(self.app_config)
             expected_errors = [
                 checks.Warning(
-                    "'django.contrib.sessions' must be on schemas that also have '%s'." % user_app, id="pgschemas.W002",
+                    "'%s' must be together with '%s' in TENANTS['%s']['APPS']."
+                    % ("django.contrib.sessions", user_app, "default"),
+                    id="pgschemas.W002",
                 )
             ]
             self.assertEqual(errors, expected_errors)

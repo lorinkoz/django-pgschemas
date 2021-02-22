e6370aad2f81c3eceebfe4599f3bc90dbdb05a77
Lorenzo Peña <lorinkoz@gmail.com>
2021-02-22 16:11:36 -0500

    Fixed bug with schema arguments in tenant commands

--- dpgs_sandbox/app_tenants/models.py
+++ dpgs_sandbox/app_tenants/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class TenantData(models.Model):
     catalog = models.ForeignKey("shared_public.Catalog", on_delete=models.CASCADE, related_name="tenant_objects")
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="tenant_objects")
+
+    active = models.BooleanField(default=True)

21f409ac389dcbd1aa15413b2016057cc8e6dc75
Lorenzo Peña <lorinkoz@gmail.com>
2021-02-22 16:29:34 -0500

    Added some tests to TestStaticTenantTestCase

--- dpgs_sandbox/tests/test_static_tenant_test_case.py
+++ dpgs_sandbox/tests/test_static_tenant_test_case.py
@@ -1,7 +1,11 @@
+from django.apps import apps
 from django.test import SimpleTestCase
 
 from django_pgschemas.test.cases import StaticTenantTestCase
 from django_pgschemas.utils import get_clone_reference
+
+BlogEntry = apps.get_model("app_blog.BlogEntry")
+User = apps.get_model("shared_common.User")
 
 
 class TestSetUpStaticTenantTestCase(SimpleTestCase):
@@ -55,7 +59,24 @@ class TestStaticTenantTestCase(StaticTenantTestCase):
 
     schema_name = "blog"
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(email="admin@localhost", display_name="Admin")
+        BlogEntry.objects.create(user=cls.user)
+
     def test_correct_set_up(self):
         self.assertTrue(self.tenant)
         self.assertEqual(self.tenant.schema_name, "blog")
         self.assertEqual(self.tenant.domain_url, "blog.test.com")
+
+    def test_random_operation1(self):
+        BlogEntry.objects.create(user=self.user)
+        BlogEntry.objects.create(user=self.user)
+        self.assertEqual(BlogEntry.objects.count(), 3)
+
+    def test_random_operation2(self):
+        BlogEntry.objects.all().delete()
+        self.assertEqual(BlogEntry.objects.count(), 0)
+
+    def test_random_operation3(self):
+        self.assertEqual(BlogEntry.objects.count(), 1)

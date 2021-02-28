28e07b9c49940dea79514c7e02afb748b0b06d56
Lorenzo Peña <lorinkoz@gmail.com>
2021-02-27 19:25:38 -0500

    Added fairly-consistent return type for SchemaDescriptor.get_primary_domain

--- dpgs_sandbox/app_main/urls.py
+++ dpgs_sandbox/app_main/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -5,4 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("", TemplateView.as_view(), name="main-home"),
     path("register/", TemplateView.as_view(), name="register"),
+    path("ping/", lambda request: HttpResponse(), name="ping"),
 ]

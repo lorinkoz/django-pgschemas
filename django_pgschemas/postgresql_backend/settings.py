970c730f7be6f857ba20fd778d86f032c5339099
Lorenzo Peña <lorinkoz@gmail.com>
2022-02-07 09:05:50 +0100

    Fixed bug with original backend database introspection (non-testable?)

--- django_pgschemas/postgresql_backend/settings.py
+++ django_pgschemas/postgresql_backend/settings.py
@@ -2,7 +2,9 @@ from importlib import import_module
 
 from django.conf import settings
 
-ORIGINAL_BACKEND = getattr(settings, "PGSCHEMAS_ORIGINAL_BACKEND", "django.db.backends.postgresql")
+BASE_BACKEND = "django.db.backends.postgresql"
+ORIGINAL_BACKEND = getattr(settings, "PGSCHEMAS_ORIGINAL_BACKEND", BASE_BACKEND)
 EXTRA_SEARCH_PATHS = getattr(settings, "PGSCHEMAS_EXTRA_SEARCH_PATHS", [])
 
+base_backend = import_module(BASE_BACKEND + ".base")
 original_backend = import_module(ORIGINAL_BACKEND + ".base")

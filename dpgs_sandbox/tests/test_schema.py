4f6116fb18403294c73f2237d297c648b4190c51
Lorenzo Peña <lorinkoz@gmail.com>
2022-12-23 13:48:27 +0100

    Fixed test about warnings

--- dpgs_sandbox/tests/test_schema.py
+++ dpgs_sandbox/tests/test_schema.py
@@ -1,16 +1,16 @@
+import warnings
+
 from django.test import SimpleTestCase
 
 
 class TestDeprecatedImports(SimpleTestCase):
     def test_schema_descriptor_warning(self):
-        import warnings
-
-        warnings.simplefilter("error")
-
-        with self.assertRaises(DeprecationWarning) as ctx:
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
             from django_pgschemas.schema import SchemaDescriptor  # noqa
 
-            self.assertEqual(str(ctx.exception), "'SchemaDescriptor' is deprecated, use 'Schema' instead")
+            self.assertEqual(len(w), 1)
+            self.assertEqual(str(w[0].message), "'SchemaDescriptor' is deprecated, use 'Schema' instead")
 
     def test_schema_descriptor_accessible(self):
         from django_pgschemas.schema import SchemaDescriptor

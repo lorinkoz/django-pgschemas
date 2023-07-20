c975798373e7fb18d03ff7d882714fed5df38942
Lorenzo Peña <lorinkoz@gmail.com>
2023-07-20 08:47:26 +0200

    Applied linters

--- dpgs_sandbox/tests/test_schema.py
+++ dpgs_sandbox/tests/test_schema.py
@@ -10,7 +10,9 @@ class TestDeprecatedImports(SimpleTestCase):
             from django_pgschemas.schema import SchemaDescriptor  # noqa
 
             self.assertEqual(len(w), 1)
-            self.assertEqual(str(w[0].message), "'SchemaDescriptor' is deprecated, use 'Schema' instead")
+            self.assertEqual(
+                str(w[0].message), "'SchemaDescriptor' is deprecated, use 'Schema' instead"
+            )
 
     def test_schema_descriptor_accessible(self):
         from django_pgschemas.schema import SchemaDescriptor

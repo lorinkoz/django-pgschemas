725919b9f4d914a72a0faaae9bff68e329623f59
Lorenzo Peña <lorinkoz@gmail.com>
2023-11-14 08:40:42 +0100

    Fixed bug

--- django_pgschemas/schema.py
+++ django_pgschemas/schema.py
@@ -7,44 +7,6 @@ from asgiref.sync import sync_to_async
 from django_pgschemas.signals import schema_activate
 
 
-def get_default_schema() -> "Schema":
-    return Schema.create("public")
-
-
-active: ContextVar["Schema"] = ContextVar("active", default=get_default_schema())
-
-
-def get_current_schema() -> "Schema":
-    active.get()
-
-
-def activate(schema: "Schema"):
-    if not isinstance(schema, Schema):
-        raise RuntimeError("'activate' must be called with a Schema descendant")
-
-    active.set(schema)
-
-    schema_activate.send(sender=Schema, schema=schema)
-
-
-def deactivate():
-    active.set(get_default_schema())
-
-    schema_activate.send(sender=Schema, schema=Schema.create("public"))
-
-
-activate_public = deactivate
-
-
-@contextmanager
-def override(schema: "Schema") -> Iterator[None]:
-    token = active.set(schema)
-
-    yield
-
-    active.reset(token)
-
-
 class Schema:
     schema_name = None
     domain_url = None
@@ -73,3 +35,41 @@ class Schema:
             active.reset(_previous_active_token)
 
     __aexit__ = sync_to_async(__exit__)
+
+
+def get_default_schema() -> "Schema":
+    return Schema.create("public")
+
+
+active: ContextVar["Schema"] = ContextVar("active", default=get_default_schema())
+
+
+def get_current_schema() -> "Schema":
+    return active.get()
+
+
+def activate(schema: "Schema") -> None:
+    if not isinstance(schema, Schema):
+        raise RuntimeError("'activate' must be called with a Schema descendant")
+
+    active.set(schema)
+
+    schema_activate.send(sender=Schema, schema=schema)
+
+
+def deactivate() -> None:
+    active.set(get_default_schema())
+
+    schema_activate.send(sender=Schema, schema=Schema.create("public"))
+
+
+activate_public = deactivate
+
+
+@contextmanager
+def override(schema: "Schema") -> Iterator[None]:
+    token = active.set(schema)
+
+    yield
+
+    active.reset(token)

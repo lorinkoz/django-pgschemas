35f91504ef5ff35225c8914c8c91662a20368d05
Lorenzo Peña <lorinkoz@gmail.com>
2023-11-15 09:56:48 +0100

    Added tests for known bugs, some failing

---
+++ sandbox/tests/bugs/test_migrations_in_base_models.py
@@ -0,0 +1,73 @@
+import warnings
+from unittest.mock import patch
+
+import pytest
+from django.apps import apps
+from django.core import management
+from django.core.management.base import CommandError
+from django.db import models
+from django.db.utils import ProgrammingError
+
+from django_pgschemas.checks import check_schema_names
+from django_pgschemas.models import TenantModel as BaseTenantModel
+from django_pgschemas.utils import get_tenant_model
+
+
+def patched_get_tenant_model(*args, **kwargs):
+    class TenantModel(BaseTenantModel):
+        dummy = models.TextField()
+
+        class Meta:
+            app_label = get_tenant_model()._meta.app_label
+
+    return TenantModel
+
+
+@pytest.mark.bug
+def test_database_checks_with_zero_migrations(transactional_db):
+    """
+    Provoke a handled ProgrammingError by migrating models from empty database.
+    """
+    management.call_command("migrate", "shared_public", "zero", verbosity=0)
+    # The goal is that the next line doesn't raise ProgrammingError
+    check_schema_names(apps.get_app_config("django_pgschemas"))
+    management.call_command("migrate", verbosity=0)
+
+
+@pytest.mark.bug
+@patch("django_pgschemas.management.commands.get_tenant_model", patched_get_tenant_model)
+def test_whowill_with_pending_migrations(db):
+    """
+    Provoke a handled ProgrammingError by running tenant command with pending model changes.
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")  # Avoid warnings about model being registered twice
+
+        with pytest.raises(CommandError) as ctx:
+            management.call_command("whowill", all_schemas=True, verbosity=0)
+
+        assert str(ctx.value) == (
+            "Error while attempting to retrieve dynamic schemas. "
+            "Perhaps you need to migrate the 'public' schema first?"
+        )
+
+
+@pytest.mark.bug
+def test_migrate_with_exclusions(db):
+    # We first unapply a migration with fake so we can reapply it without fake
+    # This should work without errors
+    management.call_command(
+        "migrate", "app_tenants", "0001_initial", fake=True, schemas=["tenant1"], verbosity=0
+    )
+
+    # We then migrate on all schemas except for tenant1, THIS IS THE CASE WE WANT TO TEST
+    # This should work without errors
+    management.call_command("migrate", all_schemas=True, excluded_schemas=["tenant1"], verbosity=0)
+
+    # If we try to global migrate now, we should get a ProgrammingError
+    with pytest.raises(ProgrammingError):
+        management.call_command("migrate", all_schemas=True, verbosity=0)
+    # We finally apply the migration again with fake
+    # This should work without errors
+
+    management.call_command("migrate", fake=True, all_schemas=True, verbosity=0)
---
+++ sandbox/tests/bugs/test_url_cache.py
@@ -0,0 +1,55 @@
+import pytest
+from django.apps import apps
+from django.test import Client
+
+from django_pgschemas.utils import get_domain_model, get_tenant_model
+
+TenantModel = get_tenant_model()
+DomainModel = get_domain_model()
+User = apps.get_model("shared_common.User")
+
+
+@pytest.fixture(autouse=True)
+def _setup(tenant1, tenant2):
+    DomainModel.objects.create(
+        tenant=tenant1, domain="everyone.localhost", folder="tenant1", is_primary=True
+    )
+    DomainModel.objects.create(
+        tenant=tenant2, domain="everyone.localhost", folder="tenant2", is_primary=True
+    )
+    with tenant1:
+        User.objects.create(email="user1@localhost", display_name="Admin")
+
+    with tenant2:
+        User.objects.create(email="user2@localhost", display_name="Admin")
+
+
+@pytest.fixture
+def client1():
+    return Client(headers={"host": "everyone.localhost"})
+
+
+@pytest.mark.bug
+class CachedTenantSubfolderBugTestCase:
+    """
+    Tests the behavior of subfolder routing regarding caching of URL patterns.
+    This test checks that a bug reported in issue #8.
+    """
+
+    def test_bug_in_cached_urls_1(self, client):
+        client.get("/tenant2/profile/advanced/")  # Provoke redirect to login on tenant2
+        buggy_response = client.get(
+            "/tenant1/profile/advanced/"
+        )  # Provoke redirect to login on tenant1
+
+        buggy_response.status_code == 302
+        buggy_response.url == "/tenant1/login/?next=/tenant1/profile/advanced/"
+
+    def test_bug_in_cached_urls_2(self, client):
+        client.get("/tenant1/profile/advanced/")  # Provoke redirect to login on tenant1
+        buggy_response = client.get(
+            "/tenant2/profile/advanced/"
+        )  # Provoke redirect to login on tenant2
+
+        buggy_response.status_code == 302
+        buggy_response.url == "/tenant2/login/?next=/tenant2/profile/advanced/"

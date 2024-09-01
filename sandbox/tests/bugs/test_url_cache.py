0adcbcdff9705c07e2093fb94ee6d4d0ce585d24
Lorenzo Peña <lorinkoz@gmail.com>
2024-09-01 10:22:19 +0200

    Fixed buggy test

--- sandbox/tests/bugs/test_url_cache.py
+++ sandbox/tests/bugs/test_url_cache.py
@@ -2,11 +2,17 @@ import pytest
 from django.apps import apps
 from django.test import Client
 
-User = apps.get_model("shared_common.User")
+
+@pytest.fixture
+def UserModel():
+    return apps.get_model("shared_common.User")
 
 
 @pytest.fixture(autouse=True)
-def _setup(tenant1, tenant2, DomainModel):
+def _setup(tenant1, tenant2, DomainModel, UserModel):
+    if DomainModel is None:
+        pytest.skip("Domain model is not in use")
+
     DomainModel.objects.create(
         tenant=tenant1, domain="everyone.localhost", folder="tenant1", is_primary=True
     )
@@ -14,38 +20,36 @@ def _setup(tenant1, tenant2, DomainModel):
         tenant=tenant2, domain="everyone.localhost", folder="tenant2", is_primary=True
     )
     with tenant1:
-        User.objects.create(email="user1@localhost", display_name="Admin")
+        UserModel.objects.create(email="user1@localhost", display_name="Admin")
 
     with tenant2:
-        User.objects.create(email="user2@localhost", display_name="Admin")
+        UserModel.objects.create(email="user2@localhost", display_name="Admin")
 
 
 @pytest.fixture
-def client1():
+def client():
     return Client(headers={"host": "everyone.localhost"})
 
 
 @pytest.mark.bug
-class CachedTenantSubfolderBugTestCase:
-    """
-    Tests the behavior of subfolder routing regarding caching of URL patterns.
-    This test checks that a bug reported in issue #8.
-    """
+def test_bug_in_cached_urls_1(client):
+    # Provoke redirect to login on tenant2
+    client.get("/tenant2/profile/advanced/")
 
-    def test_bug_in_cached_urls_1(self, client):
-        client.get("/tenant2/profile/advanced/")  # Provoke redirect to login on tenant2
-        buggy_response = client.get(
-            "/tenant1/profile/advanced/"
-        )  # Provoke redirect to login on tenant1
+    # Provoke redirect to login on tenant1
+    buggy_response = client.get("/tenant1/profile/advanced/")
 
-        buggy_response.status_code == 302
-        buggy_response.url == "/tenant1/login/?next=/tenant1/profile/advanced/"
+    assert buggy_response.status_code == 302
+    assert buggy_response.url == "/tenant1/login/?next=/tenant1/profile/advanced/"
 
-    def test_bug_in_cached_urls_2(self, client):
-        client.get("/tenant1/profile/advanced/")  # Provoke redirect to login on tenant1
-        buggy_response = client.get(
-            "/tenant2/profile/advanced/"
-        )  # Provoke redirect to login on tenant2
 
-        buggy_response.status_code == 302
-        buggy_response.url == "/tenant2/login/?next=/tenant2/profile/advanced/"
+@pytest.mark.bug
+def test_bug_in_cached_urls_2(client):
+    # Provoke redirect to login on tenant1
+    client.get("/tenant1/profile/advanced/")
+
+    # Provoke redirect to login on tenant2
+    buggy_response = client.get("/tenant2/profile/advanced/")
+
+    assert buggy_response.status_code == 302
+    assert buggy_response.url == "/tenant2/login/?next=/tenant2/profile/advanced/"
